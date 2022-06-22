359d0c0434622c125c96f35855c36762ac07e8a8
ragak <ragak@users.noreply.github.com>
2022-06-22 12:23:30 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-install_requires = ['pandas==1.3','numpy==1.21.2','configargparse','seaborn==0.11.2','scipy==1.7.1','scikit-learn==1.0','matplotlib==3.4.3', 'xgboost==1.4.2', 'pyinstaller==3.6', 'libomp==11.1.0']
+install_requires = ['pandas==1.3','numpy==1.22','configargparse','seaborn==0.11.2','scipy==1.7.1','scikit-learn==1.0','matplotlib==3.4.3', 'xgboost==1.4.2', 'pyinstaller==3.6', 'libomp==11.1.0']
 #install_requires=[]
 
 setup(
